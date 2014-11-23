2f52429843720eaf7067a90649ab64171b615c73
dozo <dohzoh@outlook.com>
2014-11-23 23:43:24 +0900

    supported controller

--- src/PhpSpec/Fuelphp/Generator/FuelphpSpecificationGenerator.php
+++ src/PhpSpec/Fuelphp/Generator/FuelphpSpecificationGenerator.php
@@ -36,6 +36,36 @@ class FuelphpSpecificationGenerator extends SpecificationGenerator
     {
         return file_get_contents(__FILE__, null, null, __COMPILER_HALT_OFFSET__);
     }
+    /**
+     * @param ResourceInterface $resource
+     * @param string            $filepath
+     *
+     * @return string
+     */
+    protected function renderTemplate(ResourceInterface $resource, $filepath)
+    {
+        $controller = <<<CONTROLLER
+    function let(Request \$request, Response \$response){
+        \$this->beConstructedWith(\$request, \$response);
+    }
+
+CONTROLLER;
+
+        $values = array(
+            '%filepath%'  => $filepath,
+            '%name%'      => $resource->getSpecName(),
+            '%namespace%' => $resource->getSpecNamespace(),
+            '%subject%'   => $resource->getSrcClassname(),
+            "%constructor%" => preg_match("!^Controller_!i", $resource->getSpecName())?$controller:"",
+        );
+
+        if (!$content = $this->getTemplateRenderer()->render('specification', $values)) {
+            $content = $this->getTemplateRenderer()->renderString($this->getTemplate(), $values);
+        }
+
+        return $content;
+    }
+
 }
 __halt_compiler();<?php
 
@@ -43,6 +73,7 @@ use PhpSpec\ObjectBehavior;
 
 class %name% extends ObjectBehavior
 {
+%constructor%
     function it_is_initializable()
     {
         $this->shouldHaveType('%subject%');
